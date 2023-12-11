965cd34d8a2f4890c4177df01a0c20e604ebf6c4
CoderDotJs <md.akash6724@gmail.com>
2023-12-11 19:31:36 +0600

    asdf

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import cors from 'cors'
 import express, { Application, Request, Response } from 'express'
+import morganMiddleware from './app/middlewares/morganMiddleware'
 const app: Application = express()
 
 //parser
 app.use(express.json())
+app.use(express.urlencoded())
 app.use(cors())
+app.use(morganMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!')
--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,16 @@
 import app from './app'
 import mongoose from 'mongoose'
 import config from './app/config'
+import logger from './app/lib/logger'
 
 async function main() {
   try {
     await mongoose.connect(config.database_url as string)
     app.listen(config.port, () => {
-      console.log(`Example app listening on port ${config.port}`)
+      logger.info(`Example app listening on port ${config.port}`)
     })
   } catch (error) {
-    console.log(error)
+    logger.error(error)
   }
 }
 
