06e6509022655fbc7b11550de4a69e21afe92595
Rony <rony@Ronys-Air.W4>
2023-12-11 19:04:18 +0600

    first commit

---
+++ src/app/config/index.ts
@@ -0,0 +1,11 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join(process.cwd(), '.env') });
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DARABASE_URL,
+  bcrypt_solt_round: process.env.BYCRIPT_SOLT_ROUND,
+};
